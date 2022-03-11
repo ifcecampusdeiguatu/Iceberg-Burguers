85a709b0516eb681739b73ca11222c3b09508d70
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-03-10 21:58:14 -0300

    inital commit

---
+++ src/main/java/br/com/icebergburguers/icebergburguers/IcebergBurguersApplication.java
@@ -0,0 +1,13 @@
+package br.com.icebergburguers.icebergburguers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebergBurguersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcebergBurguersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/icebergburguers/icebergburguers/IcebergBurguersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icebergburguers.icebergburguers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcebergBurguersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
